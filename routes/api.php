f5bc746432bf745a13ff1ee6b610fa370042ba3c
mohamed202100 <raafetmohamed0@gmail.com>
2026-01-26 22:52:50 +0200

    Implement full authentication system

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,53 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+
+
+use App\Http\Controllers\Auth\OtpController;
+use App\Http\Controllers\Auth\SocialAuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'login'])
+    ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth:sanctum', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth:sanctum', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'logout'])
+    ->middleware('auth:sanctum')
+    ->name('logout');
+
+Route::delete('/user/destroy', [UserController::class, 'destroy'])
+    ->middleware('auth:sanctum');
+
+
+
+Route::post('/verify-otp', [OtpController::class, 'verify']);
+Route::post('/verify-otp-reset', [OtpController::class, 'verifyResetOtp']);
+Route::post('/resend-otp', [OtpController::class, 'resendOtp']);
+
+
+
+Route::get('/auth/google/redirect', [SocialAuthController::class, 'redirectToGoogle']);
+Route::get('/auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback']);
